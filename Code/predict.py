3d230d9f3fb162f10265e457d1661385dde85f2b
Saurav Jayakumar <68680631+SauravSJK@users.noreply.github.com>
2023-06-05 01:33:11 -0700

    Update predict.py

--- Code/predict.py
+++ Code/predict.py
@@ -16,6 +16,7 @@ def predict(job_dir="..", file_name="/UTKFace/48_0_0_20170120134009260.jpg.chip.
         os.system("gdown --fuzzy \"https://drive.google.com/file/d/1WmjpI3AhY_YHbvXCjOeYKbuHGuFgjEPx/view\"")
         os.system("tar -xf checkpoint.zip")
         os.system("rm checkpoint.zip")
+        os.system("mv checkpoint ../")
     gen = load_model(job_dir + "/checkpoint/generator/")
 
     if not os.path.exists(job_dir + file_name):
