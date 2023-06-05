0051c006dddb47128f08cfd8c22d1d0cd7d68803
Saurav Jayakumar <68680631+SauravSJK@users.noreply.github.com>
2023-06-05 00:18:11 -0700

    Update predict.py

--- Code/predict.py
+++ Code/predict.py
@@ -11,8 +11,20 @@ from tensorflow.keras.losses import mean_squared_error
 
 
 def predict(job_dir="..", file_name="/UTKFace/48_0_0_20170120134009260.jpg.chip.jpg"):
+    if not os.path.exists(job_dir + "/checkpoint/generator/"):
+        print("Downloading model")
+        os.system("gdown --fuzzy \"https://drive.google.com/drive/folders/1WbHOMngvKUR2iOntC77oSijkUlMxL6Tw?usp=sharing\"")
     gen = load_model(job_dir + "/checkpoint/generator/")
+
+    if not os.path.exists(job_dir + file_name):
+        print("Downloading the dataset")
+        os.system("gdown --fuzzy \"https://drive.google.com/file/d/0BxYys69jI14kYVM3aVhKS1VhRUk/view?usp=share_link&resourcekey=0-dabpv_3J0C0cditpiAfhAw\"")
+        os.system("tar -xf UTKFace.tar.gz")
+        os.system("rm UTKFace.tar.gz")
+        os.system("mv UTKFace ../")
     img = np.array(Image.open(job_dir + file_name).resize((200, 200)))
+    source_age = int(file_name.split("/")[2].split("_")[0])
+    print("Source age = " + str(source_age))
     target_age = int(input("Enter the target age: "))
 
     if target_age <= 5:
@@ -51,27 +63,9 @@ def predict(job_dir="..", file_name="/UTKFace/48_0_0_20170120134009260.jpg.chip.
     output_image = np.rint(output_image.numpy()[0]).astype(int)
     output_image = np.clip(output_image, 0, 255).astype(np.uint8)
 
-    dis = load_model(job_dir + "/checkpoint/discriminator/")
-    prediction = dis(output_image[None, :, :, :])
-    print(prediction)
-
-    _, output_image_1 = gen([output_image[None, :, :, :], np.array([7])[None, :]])
-    output_image_1 = np.rint(output_image_1.numpy()[0]).astype(int)
-    output_image_1 = np.clip(output_image_1, 0, 255).astype(np.uint8)
-
-    reconstruction_loss = tf.reduce_sum(
-        tf.sqrt(
-            mean_squared_error(
-                Flatten()(img[0].astype(float)),
-                Flatten()(output_image_1.astype(float)))))
-
-    print(reconstruction_loss)
-
     im = Image.fromarray(output_image).filter(ImageFilter.SMOOTH_MORE)
-    im1 = Image.fromarray(output_image_1).filter(ImageFilter.SMOOTH_MORE)
     im.save(job_dir + "/Result.jpg")
-    im1.save(job_dir + "/Result1.jpg")
 
 
-if __name__ == 'main':
+if __name__ == "__main__":
     predict()
